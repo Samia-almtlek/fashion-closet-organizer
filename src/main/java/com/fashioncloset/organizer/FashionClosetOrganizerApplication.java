9c15b8970ec1ef382391bc430548cd9ad223de16
Samia-almtlek <samia.almoutlak@gmail.com>
2024-11-29 16:11:17 +0100

    Initial commit

---
+++ src/main/java/com/fashioncloset/organizer/FashionClosetOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.fashioncloset.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionClosetOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionClosetOrganizerApplication.class, args);
+	}
+
+}
