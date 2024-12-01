41aba67e39eb1f425d5b02ffb94450deed1a7946
Samia-almtlek <samia.almoutlak@gmail.com>
2024-12-01 23:05:46 +0100

    Add ClothingItem entity

---
+++ src/main/java/com/fashioncloset/organizer/models/ClothingItem.java
@@ -0,0 +1,59 @@
+package com.fashioncloset.organizer.models;
+
+
+// Importing the necessary libraries to work with JPA (Java Persistence API)
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity  //This annotation marks the class as a database entity
+public class ClothingItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String category;
+    private String color;
+    private String season;
+    private String imagePath;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+}
